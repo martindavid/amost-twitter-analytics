0b8929cec1e8f381b67142c816357923c632505c
wusheng <emmawu1027@gmail.com>
2017-04-13 11:31:30 +1000

    Add files via upload

--- twitter-harvester/app/streaming.py
+++ twitter-harvester/app/streaming.py
@@ -3,26 +3,33 @@ import json
 from time import sleep
 from TwitterAPI import TwitterAPI
 from tweepy.streaming import StreamListener
+import logging
 from app.db import DB, Keyword, TwitterToken
 from app.tweet_store import TweetStore
-import settings
-
-consumer_key = 'nyR8ejR7Z7LftoDYRoyusn2jg'
-consumer_secret = 'lLlxxKAOd3DyIGTLVgq1Mwy8hOnsGPCTroBhAIbgqB31Dbp0WT'
-access_token_key = '851934201506971648-TTEeRvC5pcTBu5GGl9LV1yqANvRMVu1'
-access_token_secret = 'tyZVYaNiPm2bMjjo4bkEuGwABUHoBITlrnWnDEENqKZSl'
-    
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token_key, access_token_secret)
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+import settings  
 
 # Create a class inheriting from StreamListener
-class TwitterStream(StreamListener):
-    def __init__(self, api = None):
-        self.api = api
+class TwitterStream(StreamListener): 
+
+    def __init__(self, group_name):
+        database = DB(settings.PG_DB_USER, settings.PG_DB_PASSWORD, settings.PG_DB_NAME)
+        database.connect()
+
+        keyword = Keyword(database.con, database.meta)
+        token = TwitterToken(database.con, database.meta)
+
+        # Set tweepy api object and authentication
+        token = token.find_by_group(group_name)
+        self.auth = tweepy.OAuthHandler(token["consumer_key"], token["consumer_secret"])
+        self.auth.set_access_token(token["access_token"], token["access_token_secret"])
+
+        self.api = tweepy.API(self.auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+        self.keyword = keyword
+        self.keyword_list = keyword.find_by_group(group_name)
+        self.tw_store = TweetStore('tweets', settings.COUCHDB_SERVER)
 
     def on_data(self, data):
-        print "Hello!!!"
+        print("Hello!!!")
         if 'in_reply_to_status' in data:
             self.on_status(data)
         elif 'delete' in data:
@@ -34,43 +41,50 @@ class TwitterStream(StreamListener):
                 return False
         elif 'warning' in data:
             warning = json.loads(data)['warnings']
-            print warning['message']
+            # print(warning['message'])
             return false
 
     def on_status(self, status):
-        print status 
+        self.tw_store.save_tweet(status)
+        # print status 
 
     def on_error(self, status):
         if status == 420:
-            print status
+            # print(status)
             self.on_timeout()
  
     def on_timeout(self):
-        print("Timeout, sleeping for 10 sec...\n")
+        # print("Timeout, sleeping for 10 sec...\n")
         time.sleep(10)
         return 
 
-def stream_crawl(keyword):   
-    loop = True
-    while (loop):
-        track = keyword
-        listen = TwitterStream(api)  
-        stream = tweepy.Stream(auth, listen) 
+    def execute(self):
+        # Execute the twitter crawler, loop into the keyword_list
+        for keyword in self.keyword_list:
+            logging.info('Crawl data for %s' % keyword["keyword"])
+            self.crawl(keyword)
 
-        print ("Twitter streaming started... ")
+    def crawl(self, keyword):   
+        loop = True
+        while (loop):
+            api = self.api
+            track = keyword 
+            stream = tweepy.Stream(self.auth, api) 
 
-        try:
-            stream.filter(track=[keyword])
-            loop = False
-        except:
-            print ("Error!...Retry after 10 sec")
-            loop = True
-            stream.disconnect()
-            sleep(10)
-            continue
+            # print ("Twitter streaming started... ")
 
-if __name__ == '__main__': 
-    stream_crawl( "python" )
+            try:
+                stream.filter(track=[keyword])
+                loop = False
+            except:
+                # print ("Error!...Retry after 10 sec")
+                loop = True
+                stream.disconnect()
+                sleep(10)
+                continue 
+
+
+
 
 
    
