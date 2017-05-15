3f18ad1712f20a53aa674716fca8bfb17f2293c8
Martin David Valentino <martin.david.siagian@gmail.com>
2017-05-15 19:47:32 +1000

    change initial data script

--- analytics/db_structure/analytics_log_initial.sql
+++ analytics/db_structure/analytics_log_initial.sql
@@ -1,8 +1,8 @@
-INSERT INTO public.analytics_log (data_timestamp, status, id) VALUES ('2017/5/6', false, 1);
-INSERT INTO public.analytics_log (data_timestamp, status, id) VALUES ('2017/5/7', false, 2);
-INSERT INTO public.analytics_log (data_timestamp, status, id) VALUES ('2017/5/8', false, 3);
-INSERT INTO public.analytics_log (data_timestamp, status, id) VALUES ('2017/5/9', false, 4);
-INSERT INTO public.analytics_log (data_timestamp, status, id) VALUES ('2017/5/10', false, 5);
-INSERT INTO public.analytics_log (data_timestamp, status, id) VALUES ('2017/5/11', false, 6);
-INSERT INTO public.analytics_log (data_timestamp, status, id) VALUES ('2017/5/12', false, 7);
-INSERT INTO public.analytics_log (data_timestamp, status, id) VALUES ('2017/5/13', false, 8);
+INSERT INTO public.analytics_log (data_timestamp, status) VALUES ('2017/5/6', false);
+INSERT INTO public.analytics_log (data_timestamp, status) VALUES ('2017/5/7', false);
+INSERT INTO public.analytics_log (data_timestamp, status) VALUES ('2017/5/8', false);
+INSERT INTO public.analytics_log (data_timestamp, status) VALUES ('2017/5/9', false);
+INSERT INTO public.analytics_log (data_timestamp, status) VALUES ('2017/5/10', false);
+INSERT INTO public.analytics_log (data_timestamp, status) VALUES ('2017/5/11', false);
+INSERT INTO public.analytics_log (data_timestamp, status) VALUES ('2017/5/12', false);
+INSERT INTO public.analytics_log (data_timestamp, status) VALUES ('2017/5/13', false);
